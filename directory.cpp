bceace156e1f61b330c3478ba7b0c4a868d4b2b8
xurbull <demicator@gmail.com>
2026-08-01 12:01:49 +0400

    added increment

--- directory.cpp
+++ directory.cpp
@@ -1,38 +1,149 @@
-#include <array>
-#include <vector>
-#include <string>
-#include <iostream>
-#include <sstream>
-#include <functional>
-#include <unordered_map>
 #include <algorithm>
+#include <fstream>
+#include <cctype>
+
+// add Composition / Flyweight Pattern (spcPlanetSpec)
+
+// add template
+
+// add a change if the person wants to check the value type in set or when using it
+
+// might change to <variant>
 
 #include "directory.hpp"
 
+#ifdef _WIN32
+#define strcasecmp _stricmp
+#endif
+
 using namespace std;
 
+// check
+bool is_number(const string& s) {
+	if (s.empty()) 
+		return false;
+	for (char c : s) {
+		if (!isdigit(c) && c != '-') 
+			return false;
+	}
+	return true;
+}
+
 // handlers
+// 
+// set logic
 string Directory::handleSet(const vector<string>& args) {
-	database[args[1]] = args[2];
+	database[args.at(1)] = make_shared<strVal>(args.at(2));
 	return "OK";
 }
 
+/*string Directory::handleLPush(const vector<string>& args) {
+	// write the list push logic here
+}*/
+
+// get logic
 string Directory::handleGet(const vector<string>& args) {
-	if (database.find(args[1]) != database.end()) {
-		return database[args[1]];
+	if (database.find(args.at(1)) != database.end()) {
+		return database[args.at(1)]->print();
 	}
 	return "Key not found";
 }
 
+// delete logic
 string Directory::handleDel(const vector<string>& args) {
-	if (database.erase(args[1])) {
+	if (database.erase(args.at(1))) {
 		return "OK";
 	}
 	return "Key not found";
 }
 
+// copy logic by reference
+string Directory::handleCopy(const vector<string>& args) {
+	if (database.find(args.at(1)) != database.end()) {
+		database[args.at(2)] = database[args.at(1)];
+		return "OK";
+	}
+	return "Key not found";
+}
+// increment logic, the int goes as string 
+string Directory::handleIncr(const vector<string>& args) {
+	
+	string key = args.at(1);
+	auto it = database.find(key);
+	if (it == database.end()) 
+		return "Key not found";
+
+	auto& db_val = it->second;
+
+	// if val is int
+	if (auto int_ptr = dynamic_cast<intVal*>(db_val.get())) {
+		int_ptr->incr();
+		return int_ptr->print();
+	}
+
+	// if val is string
+	if (auto str_ptr = dynamic_cast<strVal*>(db_val.get())) {
+		string org_str = str_ptr->print();
+
+		if (is_number(org_str)) {
+			int new_val = stoi(org_str) + 1;
+
+			// morphing
+			database[key] = make_shared<intVal>(new_val);
+			return to_string(new_val);
+		}
+	}
+	return "WRONGTYPE Operation against a key holding the wrong kind of value";
+}
+
+// exit logic
 string Directory::handleExit(const vector<string>& args) {
 	exit(0);
+	return "Exit";
+}
+
+// persistence handlers
+
+// save logic
+string Directory::handleSave(const vector<string>& args) {
+	ofstream os("database.txt");
+	if (!os) return "Error: Could not open file";
+
+	for (const auto& [key, db_val] : database) {
+		// Formatted like your CSV: key;type;value
+		os << key << ";" << db_val->get_type() << ";";
+		db_val->send_to(os);
+		os << "\n";
+	}
+	return "Database Saved to Disk!";
+}
+
+// load logic
+string Directory::handleLoad(const vector<string>& args) {
+	ifstream is("database.txt");
+	if (!is) return "Error: Could not open file";
+
+	database.clear(); // Clear current RAM
+	string line;
+
+	while (getline(is, line)) {
+		if (line.empty()) continue;
+		istringstream ss(line);
+		string key, type, value;
+
+		// Splitting using the ';' delimiter
+		getline(ss, key, ';');
+		getline(ss, type, ';');
+		getline(ss, value);
+
+		if (type == "string") {
+			database[key] = make_shared<strVal>(value);
+		}
+		else if (type == "int") {
+			database[key] = make_shared<intVal>(stoi(value));
+		}
+	}
+	return "Database Loaded from Disk!";
 }
 
 // constructor
@@ -40,7 +151,11 @@ Directory::Directory() {
 	commandHandlers["set"] = { 3, [this](const vector<string>& args) { return handleSet(args); } };
 	commandHandlers["get"] = { 2, [this](const vector<string>& args) { return handleGet(args); } };
 	commandHandlers["del"] = { 2, [this](const vector<string>& args) { return handleDel(args); } };
+	commandHandlers["incr"] = { 2, [this](const vector<string>& args) { return handleIncr(args); } };
+	commandHandlers["copy"] = { 3, [this](const vector<string>& args) { return handleCopy(args); } };
 	commandHandlers["exit"] = { 1, [this](const vector<string>& args) { return handleExit(args); } };
+	commandHandlers["save"] = { 1, [this](const vector<string>& args) { return handleSave(args); } };
+	commandHandlers["load"] = { 1, [this](const vector<string>& args) { return handleLoad(args); } };
 }
 
 
@@ -50,8 +165,9 @@ string Directory::execute(const vector<string>& args) {
 
 	string command = args[0];
 
+	// normalize to lowercase for case-insensitive matching
 	transform(command.begin(), command.end(), command.begin(),
-		[](unsigned char c) { return tolower(c); });
+		[](unsigned char c) { return std::tolower(c); });
 
 	if (commandHandlers.find(command) != commandHandlers.end()) {
 		Command cmd = commandHandlers[command];
@@ -59,7 +175,16 @@ string Directory::execute(const vector<string>& args) {
 		if (args.size() != cmd.req_args) {
 			return "Error: wrong number of arguments for '" + command + "' command";
 		}
-		return cmd.handler(args);
+		
+		try {
+			return cmd.handler(args);
+		}
+		catch (const std::out_of_range& e) {
+			return "Internal Server Error: Memory Out of Bounds";
+		}
+		catch (const std::exception& e) {
+			return string("Internal Server Error: ") + e.what();
+		}
 
 	}
 	return "Unknown command";
@@ -68,7 +193,7 @@ string Directory::execute(const vector<string>& args) {
 // print
 void Directory::debugPrint() {
 	for (const auto& [k, v] : database) {
-		cout << k << ": " << v << "\n";
+		cout << k << ": " << v->print() << "\n";
 	}
 }
 
--- directory.hpp
+++ directory.hpp
@@ -5,24 +5,77 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <memory>
 #include <functional>
 #include <unordered_map>
+
 
 struct Command {
 	int req_args;
 	std::function<std::string(const std::vector<std::string>&)> handler;
 };
 
+class dataVal {
+public:
+    virtual ~dataVal() = default;
+
+	virtual std::string print() const = 0;
+	virtual std::string get_type() const = 0;
+
+    virtual void send_to(std::ostream& os) const = 0;
+};
+
+class strVal : public dataVal {
+    std::string _value;
+public:
+	strVal(const std::string& val) : _value(val) {}
+
+	std::string print() const override { return _value; }
+	std::string get_type() const override { return "string"; }
+
+    void send_to(std::ostream& os) const override { os << _value; }
+};
+
+class intVal : public dataVal {
+    int _value;
+public:
+    intVal(const int& val) : _value(val) {}
+
+    void incr() { _value++; }
+
+    std::string print() const override { return std::to_string(_value); }
+    std::string get_type() const override { return "int"; }
+
+    void send_to(std::ostream& os) const override { os << _value; }
+};
+
+// for list
+/*class strVal : public dataVal {
+    std::string _value;
+public:
+    strVal(const std::string& val) : _value(val) {}
+
+    std::string print() const override { return _value; }
+    std::string get_type() const override { return "string"; }
+};*/
+
 class Directory {
 private:
-    std::unordered_map<std::string, std::string> database;
+    std::unordered_map<std::string, std::shared_ptr<dataVal>> database;
     std::unordered_map<std::string, Command> commandHandlers;
 
     // handler 
     std::string handleSet(const std::vector<std::string>& args);
+    std::string handleLPush(const std::vector<std::string>& args);
     std::string handleGet(const std::vector<std::string>& args);
     std::string handleDel(const std::vector<std::string>& args);
+    std::string handleCopy(const std::vector<std::string>& args);
+    std::string handleIncr(const std::vector<std::string>& args);
+
     std::string handleExit(const std::vector<std::string>& args);
+
+    std::string handleSave(const std::vector<std::string>& args);
+    std::string handleLoad(const std::vector<std::string>& args);
 
 public:
     Directory();
